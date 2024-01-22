6b880aef138a4d70080e67441bb2a41e7de44337
puly garcia <pulygarcia09@gmail.com>
2024-01-22 18:56:18 -0300

    go to login button when start in home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,10 +20,13 @@ const userStore = useUserStore();
       <img src="/scissors.png" alt="scissors" class="w-15 h-15 animate-pulse mx-auto mt-5">
 
       <nav class="flex flex-col gap-4 items-center justify-center mt-20 text-white">
-          <RouterLink :to="{name: 'user-appointments'}" class="font-bold bg-[#232323a3] px-3 py-1 rounded-md hover:bg-[#232323]">Mis turnos</RouterLink>
-
-          <RouterLink :to="{name: 'new-appointment'}" class="font-bold uppercase bg-blue-600 px-3 py-1 rounded-md hover:bg-blue-700 transition">
-              Nueva cita
+          <RouterLink :to="{name: 'auth-login'}" class="font-bold bg-blue-600 px-3 py-2 rounded-md hover:bg-blue-700 flex gap-2 items-center">
+            Iniciar sesión 
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              </svg>
+            </span>
           </RouterLink>
       </nav>
 
